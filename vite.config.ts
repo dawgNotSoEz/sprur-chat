7805c835fc79f70c960e3a595276edc7041bcb61
Savitendr Singh <paramsavi06@gmail.com>
2026-06-10 16:48:30 +0530

    Fix: move adapter-node config into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-node';
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
+			adapter: adapter(),
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries
 				runes: ({ filename }) =>
